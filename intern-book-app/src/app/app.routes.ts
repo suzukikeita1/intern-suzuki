550bacc4e052e2b28889272a9b195fec4bcc0940
suzukikeita1 <keita151105@gmail.com>
2024-02-26 10:58:01 +0900

    toolbarのコンポーネントを作成する

--- intern-book-app/src/app/app.routes.ts
+++ intern-book-app/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes,RouterModule } from '@angular/router';
 import { BookListComponent } from './pages/book-list/book-list.component';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { NgModule } from '@angular/core';
 
 
@@ -8,14 +7,12 @@ import { NgModule } from '@angular/core';
 export const routes: Routes = [
     { path: '', redirectTo: '/book-list', pathMatch: 'full' },
     { path: 'book-list', component: BookListComponent },
-    { path: 'toolbar', component: ToolbarComponent }
 ];
 
 @NgModule({
     imports: [
         RouterModule.forRoot(routes),
         BookListComponent,
-        ToolbarComponent
     ],
     exports: [RouterModule],
 })
